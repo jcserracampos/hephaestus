be572867b90930b3c241c27520cf8e7ef596517a
Júlio Campos <jcserracampos@gmail.com>
2021-03-27 21:27:10 -0300

    Initial forge

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
-  });
+  // app.on("issues.opened", async (context) => {
+  //   const issueComment = context.issue({
+  //     body: "Thanks for opening this issue!",
+  //   });
+  //   return context.octokit.issues.createComment(issueComment);
+  // });
+
+  app.on("push", async (context) => {
+    context.log.info({ event: context.name, action: context.payload.action, ref: context.payload.ref });
+  })
 
   // For more information on building apps:
   // https://probot.github.io/docs/
